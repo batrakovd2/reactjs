06142f3ec851f19718a630d5796db35e5996c3bf
batrakovd2 <batrakovd2@gmail.com>
2022-06-22 23:51:27 +0500

    get post list api

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["user_id", "attachment", "content", "like"];
+
+    public function getPostList($limit = 3) {
+        $list = Post::orderBy('name')
+            ->take($limit)
+            ->get();
+        return $list;
+    }
+
 }
